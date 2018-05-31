2a2d5a29e5c291631093f770b3c6b2925895fc76
xuejuntao <xuejuntao@baidu.com>
2018-05-31 21:39:15 +0800

    fix

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ module.exports = {
         }).catch(error => {
             console.error('[ral]call failed:error:%s', error.stack);
             if (error.code === 'ETIMEDOUT' || error.code === 'ESOCKETTIMEDOUT') {
-                console.error('[rpc] timeout opt:%j', options);
+                console.error('[rpc] timeout opt:%j', opt);
                 return Promise.reject(new Error('timeout'));
             }
             console.log('[rpc]using:%d', now() - start);
@@ -88,7 +88,7 @@ module.exports = {
         }).catch(function(error) {
             console.error('[ral]call failed:error:%s', error.stack);
             if (error.code === 'ETIMEDOUT' || error.code === 'ESOCKETTIMEDOUT') {
-                console.error('[rpc] timeout opt:%j', options);
+                console.error('[rpc] timeout opt:%j', opt);
                 return Promise.reject(new Error('timeout'));
             }
             console.log('[rpc]using:%d', now() - start);
